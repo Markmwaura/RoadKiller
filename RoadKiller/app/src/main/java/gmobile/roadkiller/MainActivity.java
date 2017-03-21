3acb90b08151eba5bee6653e2c1a43141e7dc57a
Mark Mwaura <markkmwaura@gmail.com>
2017-03-21 23:11:29 +0300

    added basegameutils module

--- RoadKiller/app/src/main/java/gmobile/roadkiller/MainActivity.java
+++ RoadKiller/app/src/main/java/gmobile/roadkiller/MainActivity.java
@@ -1,8 +1,18 @@
 package gmobile.roadkiller;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
 /**
  * Created by mark on 3/20/17.
  */
 
-public class MainActivity {
+public class MainActivity extends Activity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
 }
---
+++ RoadKiller/basegameutils/src/main/java/gmobile/basegameutils/BaseGameActivity.java
@@ -0,0 +1,8 @@
+package gmobile.basegameutils;
+
+/**
+ * Created by mark on 3/21/17.
+ */
+
+public class BaseGameActivity {
+}
